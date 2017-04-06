cb9ff8bc6735521ad5a6a17e139af8be7afe4287
Pranay Prakash <pranay.gp@gmail.com>
2017-04-06 17:05:30 -0500

    navbar tabs

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   }
 
   return (
-    <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+    <Nav bsStyle="tabs" justified activeKey={1} onSelect={handleSelect}>
       <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
       <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
       <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
