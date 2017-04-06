e3eadff44b1f96f2fa9f208d00c82419253f7ce4
Pranay Prakash <pranay.gp@gmail.com>
2017-04-06 17:01:39 -0500

    bootstrap navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,27 @@ import logo from './logo.svg';
 import { Grid, Row, Col } from 'react-bootstrap'
 import './App.css';
 
+import NavBar from './components/NavBar.js'
+
 class App extends Component {
   render() {
     return (
-      <Grid>
-        <Row>
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-        </Row>
-        <Row>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </Row>
-      </Grid>
+      <div>
+        <NavBar />
+        <Grid>
+          <Row>
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+          </Row>
+          <Row>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Nav, NavItem } from 'react-bootstrap'
+
+const NavBar = () => {
+  function handleSelect(selectedKey) {
+    alert('selected ' + selectedKey);
+  }
+
+  return (
+    <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+      <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
+      <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
+      <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+    </Nav>
+  );
+};
+
+export default NavBar;
