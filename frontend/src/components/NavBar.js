175a2747ec4fc830ae2b9bf86a283ac089daf6f2
Pranay Prakash <pranay.gp@gmail.com>
2017-04-07 11:43:25 -0500

    nav bar reflects current active page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,18 +9,19 @@ const NavBar = (props) => {
   function handleSelect(selectedKey) {
     props.changePage(selectedKey)
     // push('/about')
+    // console.log(props.location)
   }
 
   return (
-    <Nav bsStyle="tabs" justified activeKey={1} onSelect={handleSelect}>
+    <Nav bsStyle="tabs" justified activeKey={props.path} onSelect={handleSelect}>
       <NavItem eventKey="/" title="Item">Home</NavItem>
-      <NavItem eventKey="/about" title="Item">NavItem 2 content</NavItem>
+      <NavItem eventKey="/about" title="Item">About</NavItem>
     </Nav>
   );
 };
 
-const mapStateToProps = state => ({
-
+const mapStateToProps = (state, ownProps) => ({
+  path: state.router.location.pathname
 })
 
 const mapDispatchToProps = dispatch => ({
